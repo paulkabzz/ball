ae8874f3a802e602bf0f75b16921d923eb42821c
paulkabzz <pkabulu05@gmail.com>
2025-06-07 20:31:04 +0200

    fixed coordinate calculations

--- src/script/script.ts
+++ src/script/script.ts
@@ -2,11 +2,15 @@ const ball = document.querySelector('.ball') as HTMLElement;
 const boud = document.querySelector('.bound') as HTMLElement;
 
 const GRAVITAIONAL_ACCELERATION: number = 9.81;
+
+                // [x, y]
 let velocity: [number, number] = [0, 0];
 
 // Boundaries
 let upperBound: number = boud.offsetTop;
-let lowerBound: number = upperBound + boud.offsetHeight;
+let lowerBound: number = upperBound - (2*boud.offsetHeight);
+let leftBound: number = boud.offsetLeft;
+let rightBound: number = leftBound + 2*boud.offsetWidth;
 
 let position: [number, number] = [0, 0];
 
@@ -19,14 +23,17 @@ const accelerate = (x: number, y: number): void => {
 }
 
 const updatePosition = (): void => {
-    position[0] += velocity[0];
+    position[0] += velocity[0]; 
     position[1] += velocity[1];
-    ball.style.top = position[0] + 'px';
-    ball.style.left = position[1] + 'px';
+
+    ball.style.left = position[0] + 'px';
+    ball.style.top = position[1] + 'px';
 }
 
 const z = setInterval(() => {
     accelerate(5, GRAVITAIONAL_ACCELERATION);
     updatePosition();
-}, 100)
+    if (position[0] >= rightBound || velocity[0] <= leftBound ) velocity[0] *= -0.95;
+    if (position[1] < lowerBound || position[1] > upperBound) velocity[1] *= -0.95;
+}, 100);
 
